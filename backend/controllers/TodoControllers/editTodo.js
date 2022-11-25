ba4326233f885c1378f2d76a9e524590af07aae0
alokVerma749 <alokverma749@gmail.com>
2022-11-25 17:04:39 +0530

    bug fixing in edittodo controller

--- backend/controllers/TodoControllers/editTodo.js
+++ backend/controllers/TodoControllers/editTodo.js
@@ -2,10 +2,13 @@ const Todo = require('../../models/todo')
 
 const editTodo = async (req, res) => {
     try {
-        const todoId = req.params.todoId
-        console.log(req.body)
-        const todo = await Todo.findByIdAndUpdate(todoId, req.body.data)
+        const { todoId } = req.params
+        const todo = await Todo.findByIdAndUpdate(todoId, req.body.title)
         console.log(todo)
+        res.status(200).json({
+            success: true,
+            message: "title renamed"
+        })
     } catch (error) {
         throw new Error("editTodo error \n " + error.message)
     }
--- frontend/src/components/AllTasks.js
+++ frontend/src/components/AllTasks.js
@@ -1,24 +1,20 @@
 import React from 'react'
 
 const AllTasks = ({ tasks }) => {
-    const handleDelete = async (e) => {
-        const taskid = e.currentTarget.getAttribute("taskid")
-        console.log(taskid)
-    }
     const allTasks = tasks.tasks
     const title = tasks.todoTitle
     return (
         <div className='w-[69vw] border-2 border-cyan-200 tracking-wider float-right'>
             <h3 className="text-center text-3xl font-semibold text-cyan-600 my-2">{title}</h3>
             {tasks && allTasks.map((task) => (
-                <div key={task._id} className=" border-0 shadow-gray-400 shadow-xl hover:shadow-cyan-500/50 outline-0 my-2 p-2 w-[50%] h-[110px] mx-auto">
+                <div key={task} className=" border-0 shadow-gray-400 shadow-xl hover:shadow-cyan-500/50 outline-0 my-2 p-2 w-[50%] h-[110px] mx-auto">
                     <p className="text-center text-xl font-semibold text-cyan-500/50 my-2">{task}</p>
                     <div className="buttons flex justify-evenly flex-row items-center p-2">
                         <div className="buttons flex justify-evenly flex-row items-center">
-                            <button todoid={task._id} className="bg-cyan-500 shadow-xl hover:shadow-cyan-500/50 text-white p-2 outline-0">Edit</button>
+                            <button className="bg-cyan-500 shadow-xl hover:shadow-cyan-500/50 text-white p-2 outline-0">Edit</button>
                         </div>
                         <div className="buttons flex justify-evenly flex-row items-center">
-                            <button onClick={handleDelete} todoid={task._id} className="bg-red-600 shadow-xl hover:shadow-red-600/500 text-white p-2 outline-0">Delete</button>
+                            <button className="bg-red-600 shadow-xl hover:shadow-red-600/500 text-white p-2 outline-0">Delete</button>
                         </div>
                     </div>
                 </div>
