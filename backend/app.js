56f423ac49513b49cdc31ffb2caec8f0780c62b7
alok verma <alokverma749@gmail.com>
2023-01-03 00:22:06 +0530

    setting up cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -10,5 +11,6 @@ const task = require('./routes/task')
 
 app.use('/todo', todo)
 app.use('/todo/task', task)
+app.use(cors())
 
 module.exports = app
